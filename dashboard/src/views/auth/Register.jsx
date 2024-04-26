42f4a4bc6c36c61dbcbf639d77dfdc3bae142a72
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-04-26 17:47:31 +0700

    TNK-005: Init state for register form

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 
 const Register = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const inputHandle = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault()
+    console.log("state", state);
+  }
+
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
       <div className="w-[350px] text-white p-2">
@@ -12,9 +30,11 @@ const Register = () => {
           <p className="text-sm mb-4 font-medium">
             Please register your account
           </p>
-          <form>
+          <form onSubmit={submit}>
             <div className="flex flex-col w-full gap-4 mb-3">
               <input
+                onChange={inputHandle}
+                value={state.name}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="text"
                 name="name"
@@ -23,6 +43,8 @@ const Register = () => {
                 required
               />
               <input
+                onChange={inputHandle}
+                value={state.email}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="email"
                 name="email"
@@ -31,6 +53,8 @@ const Register = () => {
                 required
               />
               <input
+                onChange={inputHandle}
+                value={state.password}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="password"
                 name="password"
@@ -41,32 +65,49 @@ const Register = () => {
             </div>
 
             <div className="flex items-center w-full gap-3 mb-3">
-                <input className="w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300 focus:ring-blue-500" type="checkbox" name="checkbox" id="checkbox" />
-                <label className="font-semibold" htmlFor="checkbox">I agree to privacy policy & terms</label>
+              <input
+                className="w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300 focus:ring-blue-500"
+                type="checkbox"
+                name="checkbox"
+                id="checkbox"
+              />
+              <label className="font-semibold" htmlFor="checkbox">
+                I agree to privacy policy & terms
+              </label>
             </div>
 
-            <button className="bg-[#6f68d1] text-white w-full hover:shadow-blue-300 p-2 rounded font-semibold">Sign Up</button>
+            <button className="bg-[#6f68d1] text-white w-full hover:shadow-blue-300 p-2 rounded font-semibold">
+              Sign Up
+            </button>
             <div className="flex items-center my-4 gap-3 justify-center">
-                <p>Already have an account ? <Link className="font-bold" to="/login">Sign In</Link></p>
+              <p>
+                Already have an account ?{" "}
+                <Link className="font-bold" to="/login">
+                  Sign In
+                </Link>
+              </p>
             </div>
 
             <div className="w-full flex justify-center items-center mb-3">
-                <div className="w-[45%] bg-slate-700 h-[1px]"></div>
-                <div className="w-[10%] flex justify-center items-center">
-                    <span className="pb-1">Or</span>
-                </div>
-                <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+              <div className="w-[10%] flex justify-center items-center">
+                <span className="pb-1">Or</span>
+              </div>
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
             </div>
 
             <div className="flex justify-center items-center gap-3">
-                <div className="w-[135px] h-[35px] flex rounded-md bg-white shadow-lg hover:shadow-blue-500/50 justify-center cursor-pointer items-center">
-                    <span><FaGoogle /></span>
-                </div>
-                <div className="w-[135px] h-[35px] flex rounded-md bg-white shadow-lg hover:shadow-blue-500/50 justify-center cursor-pointer items-center">
-                    <span><FaFacebook /></span>
-                </div>
+              <div className="w-[135px] h-[35px] flex rounded-md bg-white shadow-lg hover:shadow-blue-500/50 justify-center cursor-pointer items-center">
+                <span>
+                  <FaGoogle />
+                </span>
+              </div>
+              <div className="w-[135px] h-[35px] flex rounded-md bg-white shadow-lg hover:shadow-blue-500/50 justify-center cursor-pointer items-center">
+                <span>
+                  <FaFacebook />
+                </span>
+              </div>
             </div>
-
           </form>
         </div>
       </div>
